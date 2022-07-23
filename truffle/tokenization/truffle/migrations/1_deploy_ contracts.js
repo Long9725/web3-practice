f808b869d73fe90f04baf106739ca6f89365f3dc
Long9725 <develop9725@gmail.com>
2022-07-23 17:42:49 +0900

    Add: Crowdsale.sol & StarDucksCappucinoTokenCrowdsale.sol

--- truffle/tokenization/truffle/migrations/1_deploy_ contracts.js
+++ truffle/tokenization/truffle/migrations/1_deploy_ contracts.js
@@ -1,5 +1,13 @@
 const StarDucksCappucinoToken = artifacts.require("StarDucksCappucinoToken");
+const StarDucksCappucinoTokenCrowdsale = artifacts.require("StarDucksCappucinoTokenCrowdsale");
 
-module.exports = function(deployer) {
-    deployer.deploy(StarDucksCappucinoToken, 100000);
+module.exports = async function(deployer) {
+    let addr = await web3.eth.getAccounts();
+
+    await deployer.deploy(StarDucksCappucinoToken, 100000);
+    await deployer.deploy(StarDucksCappucinoTokenCrowdsale, 1, addr[0], StarDucksCappucinoToken.address);
+
+    let instance = await StarDucksCappucinoToken.deployed();
+
+    await instance.transfer(StarDucksCappucinoTokenCrowdsale.address, 100000);
 }
--- truffle/tokenization/truffle/test/StarDucksCappucinoToken.test.js
+++ truffle/tokenization/truffle/test/StarDucksCappucinoToken.test.js
@@ -6,12 +6,14 @@ const chaiBN = require("chai-bn")(BN);
 chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised");
-const _deploy_Contracts = require("../migrations/1_deploy_ contracts");
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+    
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("All tokens should be in my account", async () => {
         let instance = await StarDucksCappucinoToken.deployed();
         let totalSupply = await instance.totalSupply();
@@ -24,8 +26,30 @@ contract("Token Test", async (accounts) => {
         // expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
 
         // chai-as-promised
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
-    })
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("is possible to send tokens between accounts", async() => {
+        const sendTokens = 1;
+
+        let instance = await StarDucksCappucinoToken.deployed();
+        let totalSupply = await instance.totalSupply();
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    // 2nd test 이후 before eact hook error 발생
+    it("is not possible to send more tokens than available in total", async() => {
+        let instance = await StarDucksCappucinoToken.deployed();
+        let balanceOfDeployer = await instance.totalSupply();
+
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+    });
 });
 
 
